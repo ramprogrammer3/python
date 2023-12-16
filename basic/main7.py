f1ec5cedc5b10252499963515e4f7f1ab0697640
ram kumar <rammaniyari@gmail.com>
2023-12-16 18:12:49 +0530

    operator

---
+++ basic/main7.py
@@ -0,0 +1,30 @@
+# import random
+
+# print(random.randrange(1,20))
+
+# print(15 + 7)
+
+# print(5 + 6)
+
+# print(10 - 45)
+# print(14 * 343)
+# print(15 / 4)
+# print(15//4)
+
+# print(15 % 4)
+
+# print(5 ** 3)
+
+
+a = 34
+b = 34
+
+print("The value of", a , "+", b , "is" , a + b)
+print("The value of", a , "-", b , "is" , a - b)
+print("The value of", a , "*", b , "is" , a * b)
+print("The value of", a , "/", b , "is" , a / b)
+
+
+
+
+
