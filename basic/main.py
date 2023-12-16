69c5334b12185b8c2b0367bf67b52ab9e703d537
ram kumar <rammaniyari@gmail.com>
2023-12-16 17:15:33 +0530

    basic data type

---
+++ basic/main.py
@@ -0,0 +1,7 @@
+
+print("hello", 7, 8)
+print("hello",7,8,sep="/")
+print("ram kumar maniyari")
+
+if 3 > 1 :
+    print("Three is greater than two")
---
+++ basic/main6.py
@@ -0,0 +1,67 @@
+
+# print("Hello , what is variable here ")
+
+# name = "ram kumar "
+# print(name)
+
+# name  = "20"
+# print(name)
+
+# num1 = 20
+# num2 = 343
+
+# sum = num1 + num2
+
+# print(sum)
+
+# # variable is a container to store value 
+# a = 2
+# print(a)
+
+# print(a)
+
+# b = "maniyari"
+# print(b)
+# b = 3432985
+# print(b)
+
+# isProgramer = True
+# print(isProgramer)
+
+
+# x = "awesome"
+
+# def myFun():
+#     print("the value of x is ", x)
+
+# myFun()    
+
+# print(type (myFun))
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+
+a = complex(6,6)
+
+print(a)
+
+list = ["apple", "banana", "cherry"]
+print(list)
+
+touple = ("apple", "banana", "cherry")
+
+print(touple)
+
+
+dict1 = {"name" : "palak","age" : 25, "IsDispline" : False}
+
+print(dict1)
