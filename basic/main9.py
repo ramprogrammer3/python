9277d8f26909277642a6d48217aa57ed3d148bbe
ram kumar <rammaniyari@gmail.com>
2023-12-17 07:57:06 +0530

    input

---
+++ basic/main9.py
@@ -0,0 +1,35 @@
+# a = "1"
+# b = "2"
+
+
+# print(a + b)
+
+# print(int(a) + int(b))
+
+# fname = "ram"
+# lname = "kumar"
+
+# print(fname.upper() , lname.upper())
+
+# c = 1.9
+# d = 8
+
+# print( c + d)
+
+# print(type (c + d))
+
+name = input("Enter your name ")
+
+print("Your name is ", name)
+
+num1 = input("Enter first number:")
+num2 = input("Enter second number : ")
+
+print("The sum is ", int(num1) + int(num2))
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
